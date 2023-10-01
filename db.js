3bd63e9bde775b5901638e9ced69eca0013aadd8
Sainath <sainath@analyticsquad4.com>
2023-10-01 21:11:57 +0530

    Db config

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2')
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '03052002',
-  database: 'first'
+  host: 'btnhobmygm6ugz0tjcmq-mysql.services.clever-cloud.com',
+  user: 'u8gmxlh2mohvktli',
+  password: 'oPEUV6oXW7eF2V2bhuBW',
+  database: 'btnhobmygm6ugz0tjcmq'
 })
 
 connection.connect(function(err) {
